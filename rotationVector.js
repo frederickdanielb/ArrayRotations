2784ec8cf883edcde1c300aab9249756abcb9ae1
Frederick Bustamante <frederickdanielb@gmail.com>
2016-09-28 21:34:18 -0300

    agregados metodos para lógica de rotación

---
+++ rotationVector.js
@@ -0,0 +1,28 @@
+let inputString = "5;4;2;7;9;3;8";
+let inputArray = inputString.split(';');
+let sizeVector = inputArray[0];
+let numRotation = inputArray[1];
+inputArray.splice(0, 2);
+
+const becual = {
+    applyRotation: (array, numberOfRotations)=> {
+        var currentArray = array;
+        for (var i = 0; i <= numberOfRotations; i++) {
+            if (i <= numberOfRotations-1) {
+                currentArray = becual.arrayRotation(currentArray);
+            } else {
+                return currentArray;
+            }
+
+        }
+    },
+    arrayRotation: (array)=> {
+        var firstPosition;
+        firstPosition = array[0];
+        array.splice(0, 1);
+        array.push(firstPosition)
+        return array;
+    }
+
+}
+console.log(becual.applyRotation(inputArray, numRotation));
